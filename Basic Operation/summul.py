82b98dfc8d7926ac9af516cceaf339633505b342
noxsine <1060751317@qq.com>
2018-04-04 00:54:45 +0800

    Add files via upload

---
+++ Basic Operation/summul.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+input1 = tf.constant(2.0)              #定义常量
+input2 = tf.constant(3.0)
+input3 = tf.constant(5.0)
+
+intermd = tf.add(input1, input2)
+mul = tf.multiply(input2, input3)
+
+with tf.Session() as sess:
+    result = sess.run([mul, intermd])  # 一次执行多个op
+    print (result)
+    print (type(result))
+    print (type(result[0]))
